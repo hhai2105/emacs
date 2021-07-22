3aa733b3941d093d63eb3cc4f455f27b565979a7
Hai Nguyen <hhai2105@gmail.com>
2021-07-22 12:34:33 -0400

    restructuring of cofig.org, added yasnippets,etc.

--- config.el
+++ config.el
@@ -1,75 +1,115 @@
+(require 'package)
 (add-to-list 'package-archives '("ORG" . "http://orgmode.org/elpa/") t)
 (add-to-list 'package-archives '("MELPA STABLE" . "http://stable.melpa.org/packages/"))
 (add-to-list 'package-archives '("MELPA" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("GNU ELPA" . "http://elpa.gnu.org/packages/"))
+(package-refresh-contents)
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
-(use-package evil
-	     :ensure t
-	     :init
-	     (setq evil-want-integration t)
-	     (setq evil-want-keybinding nil)
-	     (setq evil-vsplit-window-right t)
-	     (setq evil-split-window-below t)
-	     (evil-mode))
-(use-package evil-collection
-	     :after evil
-	     :ensure t
-	     :config
-	     (evil-collection-init))
+;; Using garbage magic hack.
+(use-package gcmh
+    :ensure t
+    :config
+    (gcmh-mode 1))
+;; Setting garbage collection threshold
+(setq gc-cons-threshold 402653184
+    gc-cons-percentage 0.6)
 
-(use-package 'all-the-icons-dired
-:ensure t)
-(use-package 'all-the-icons-ibuffer
-:ensure t)
-(use-package 'all-the-icons-dired
-:ensure t)
-(use-package 'all-the-icons-dired
-:ensure t)
-(use-package 'all-the-icons-dired
-:ensure t)
+;; Profile emacs startup
+(add-hook 'emacs-startup-hook
+	(lambda ()
+	    (message "*** Emacs loaded in %s with %d garbage collections."
+		    (format "%.2f seconds"
+			    (float-time
+			    (time-subtract after-init-time before-init-time)))
+		    gcs-done)))
+
+  ;; Silence compiler warnings as they can be pretty disruptive (setq comp-async-report-warnings-errors nil)
+
+(setq backup-directory-alist '(("." . "~/.emacs.d/saves/")))
+
+(use-package evil
+    :ensure t
+    :init
+    (setq evil-want-integration t)
+    (setq evil-want-keybinding nil)
+    (setq evil-vsplit-window-right t)
+    (setq evil-split-window-below t)
+    (evil-mode))
+(use-package evil-collection
+    :after evil
+    :ensure t
+    :config
+    (evil-collection-init))
 
 (use-package which-key
-    :ensure t)
+	:ensure t)
 (which-key-mode)
 
+(use-package general
+     :ensure t
+     :config
+(general-evil-setup t))
+;; (nvmap :prefix "SPC"
+
 (menu-bar-mode -1)
-     (tool-bar-mode -1)
+(tool-bar-mode -1)
 (scroll-bar-mode -1)
 
 (use-package doom-themes
-	:ensure t)
+    :ensure t)
 (setq doom-themes-enable-bold t
     doom-themes-enable-italics t)
 (load-theme 'doom-outrun-electric t)
 
+(use-package all-the-icons
+    :ensure t)
+
 (use-package doom-modeline
- :ensure t)
+     :ensure t)
 (doom-modeline-mode 1)
 
 (global-display-line-numbers-mode 1)
 (global-visual-line-mode t)
 
 (set-face-attribute 'default nil
+    :font "Noto Sans Mono 15"
+    :weight 'medium)
+(set-face-attribute 'variable-pitch nil
 	:font "Noto Sans Mono 15"
-      :weight 'medium)
-    (set-face-attribute 'variable-pitch nil
-	:font "Noto Sans Mono 15"
-      :weight 'medium)
-    (set-face-attribute 'fixed-pitch nil
-	:font "Noto Sans Mono 15"
-      :weight 'medium)
-  ;;(setq-default line-spacing 0.10)
+    :weight 'medium)
+(set-face-attribute 'fixed-pitch nil
+    :font "Noto Sans Mono 15"
+    :weight 'medium)
+;;(setq-default line-spacing 0.10)
 (add-to-list 'default-frame-alist '(font . "Noto Sans Mono 15"))
 
-(use-package general
-:ensure t
-:config
-(general-evil-setup t))
-;; (nvmap :prefix "SPC"
+(add-hook 'org-mode-hook 'org-indent-mode)
+(setq org-edit-src-content-indentation 0
+    org-src-tab-acts-natively t
+    org-src-preserve-indentation nil)
 
-(global-set-key (kbd "C-=") 'text-scale-increase)
-(global-set-key (kbd "C--") 'text-scale-decrease)
+(use-package org-bullets
+  :ensure t)
+(add-hook 'org-mode-hook (lambda() (org-bullets-mode 1)))
+
+(setq org-startup-folded t)
+
+(use-package toc-org
+    :ensure t
+    :commands toc-org-enable
+    :init (add-hook 'org-mode-hook 'toc-org-enable))
+
+(use-package yasnippet
+    :ensure t)
+
+(use-package yasnippet-snippets
+
+(use-package emojify
+    :ensure t
+    :hook (after-init . global-emojify-mode))
+
+(setq gc-cons-threshold (* 2 1000 1000))
--- init.el
+++ init.el
@@ -7,7 +7,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(all-the-icons-dired ## doom-modeline doom-modelines evil)))
+ '(package-selected-packages
+   '(yasnippet toc-org org-bullets all-the-icons-dired ## doom-modeline doom-modelines evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
