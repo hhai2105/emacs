a07662efb018d3f3cca537449af8a57a779584e8
Hai Nguyen <hhai2105@gmail.com>
2021-07-24 03:57:46 -0400

    added yasnippet, rainbow delims, keybindings

--- config.el
+++ config.el
@@ -1,17 +1,11 @@
 (require 'package)
-(add-to-list 'package-archives '("ORG" . "http://orgmode.org/elpa/") t)
-(add-to-list 'package-archives '("MELPA STABLE" . "http://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("MELPA" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("GNU ELPA" . "http://elpa.gnu.org/packages/"))
-(package-refresh-contents)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
 
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
+(setq use-package-always-ensure t)
 
 ;; Using garbage magic hack.
 (use-package gcmh
-    :ensure t
     :config
     (gcmh-mode 1))
 ;; Setting garbage collection threshold
@@ -29,51 +23,32 @@
 
   ;; Silence compiler warnings as they can be pretty disruptive (setq comp-async-report-warnings-errors nil)
 
-(setq backup-directory-alist '(("." . "~/.emacs.d/saves/")))
+(setq backup-directory-alist '(("." . "~/.config/emacs/saves/")))
 
-(use-package evil
-    :ensure t
-    :init
-    (setq evil-want-integration t)
-    (setq evil-want-keybinding nil)
-    (setq evil-vsplit-window-right t)
-    (setq evil-split-window-below t)
-    (evil-mode))
-(use-package evil-collection
-    :after evil
-    :ensure t
-    :config
-    (evil-collection-init))
-
-(use-package which-key
-	:ensure t)
-(which-key-mode)
-
-(use-package general
-     :ensure t
-     :config
-(general-evil-setup t))
-;; (nvmap :prefix "SPC"
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
-(use-package doom-themes
-    :ensure t)
+(use-package doom-themes)
 (setq doom-themes-enable-bold t
     doom-themes-enable-italics t)
 (load-theme 'doom-outrun-electric t)
 
-(use-package all-the-icons
-    :ensure t)
-
-(use-package doom-modeline
-     :ensure t)
+(use-package doom-modeline)
 (doom-modeline-mode 1)
 
+(column-number-mode)
 (global-display-line-numbers-mode 1)
 (global-visual-line-mode t)
+
+(dolist (mode '(term-mode-hook
+        eshell-mode-hook))
+    (add-hook mode (lambda() (display-line-numbers-mode 0))))
+
+(use-package rainbow-delimiters
+:hook (prog-mode . rainbow-delimiters-mode))
 
 (set-face-attribute 'default nil
     :font "Noto Sans Mono 15"
@@ -87,29 +62,91 @@
 ;;(setq-default line-spacing 0.10)
 (add-to-list 'default-frame-alist '(font . "Noto Sans Mono 15"))
 
-(add-hook 'org-mode-hook 'org-indent-mode)
-(setq org-edit-src-content-indentation 0
-    org-src-tab-acts-natively t
-    org-src-preserve-indentation nil)
+(use-package evil
+    :init
+    (setq evil-want-integration t)
+    (setq evil-want-keybinding nil)
+    (setq evil-vsplit-window-right t)
+    (setq evil-split-window-below t)
+    (evil-mode))
+(use-package evil-collection
+    :after evil
+    :config
+    (evil-collection-init))
 
-(use-package org-bullets
-  :ensure t)
+(use-package which-key)
+(which-key-mode)
+
+(use-package general
+     :config
+(general-evil-setup t))
+
+(use-package ivy
+    :diminish
+    :bind (("C-s" . swiper)
+            :map ivy-minibuffer-map
+            ("TAB" . ivy-alt-done)
+            ("C-l" . ivy-alt-done)
+            ("C-j" . ivy-next-line)
+            ("C-k" . ivy-previous-line)
+            :map ivy-switch-buffer-map
+            ("C-k" . ivy-previous-line)
+            ("C-l" . ivy-done)
+            ("C-d" . ivy-switch-buffer-kill)
+            :map ivy-reverse-i-search-map
+            ("C-k" . ivy-previous-line)
+            ("C-d" . ivy-reverse-i-search-kill))
+    :config
+    (ivy-mode 1))
+
+(use-package counsel)
+
+(use-package projectile)
+
+(use-package all-the-icons)
+
+(use-package sudo-edit)
+
+(use-package dashboard)
+
+(nvmap :states '(normal visual) :keymaps 'override :prefix "SPC"
+    "."     '(find-file :which-key "Find file")
+    "f f"   '(find-file :which-key "Find file")
+    "f r"   '(counsel-recentf :which-key "Recent files")
+    "f s"   '(save-buffer :which-key "Save file")
+    "f u"   '(sudo-edit-find-file :which-key "Sudo find file")
+    "f y"   '(dt/show-and-copy-buffer-path :which-key "Yank file path")
+    "f C"   '(copy-file :which-key "Copy file")
+    "f D"   '(delete-file :which-key "Delete file")
+    "f R"   '(rename-file :which-key "Rename file")
+    "f S"   '(write-file :which-key "Save file as...")
+    "f U"   '(sudo-edit :which-key "Sudo edit file"))
+
+(add-hook 'org-mode-hook 'org-indent-mode)
+(setq org-src-tab-acts-natively t
+    org-src-preserve-indentation nil
+    org-edit-src-content-indentation 0)
+
+(use-package org-bullets)
 (add-hook 'org-mode-hook (lambda() (org-bullets-mode 1)))
 
 (setq org-startup-folded t)
 
+(setq org-src-fontify-natively t
+    org-confirm-babel-evaluate nil)
+
 (use-package toc-org
-    :ensure t
     :commands toc-org-enable
     :init (add-hook 'org-mode-hook 'toc-org-enable))
 
-(use-package yasnippet
-    :ensure t)
+(use-package yasnippet)
+(yas-global-mode 1)
 
-(use-package yasnippet-snippets
+(use-package yasnippet-snippets)
+
+(use-package haskell-mode)
 
 (use-package emojify
-    :ensure t
     :hook (after-init . global-emojify-mode))
 
 (setq gc-cons-threshold (* 2 1000 1000))
--- init.el
+++ init.el
@@ -8,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(yasnippet toc-org org-bullets all-the-icons-dired ## doom-modeline doom-modelines evil)))
+   '(dashboard sudo-edit rainbow-delimiters projectile auctex yasnippet toc-org org-bullets all-the-icons-dired ## doom-modeline doom-modelines evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
